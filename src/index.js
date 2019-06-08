03fdd55ec7485dd662cea9122b4e94d210843550
ashley.fernandes <ashley.fernandes@rksv.in>
2019-06-08 16:06:26 +0530

    added new features: get single name and getNameById

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
-let superHeroNames = require('./superhero.json')
-let uniqueRandomArray = require('unique-random-array');
+const superHeroNamesList = require("./superhero.json");
+const uniqueRandomArray = require("unique-random-array");
+
+let allSuperHeroNames = superHeroNamesList.map(superhero => {
+  return superhero.name;
+});
+
+let superHeroById = id => {
+  let superHeroObj;
+  let stringId;
+  try {
+    stringId = String(id);
+  } catch (err) {
+    console.error("Please enter a number");
+  }
+
+  if (id > 0 && id <= superHeroNamesList.length) {
+    superHeroObj = superHeroNamesList.find(superhero => {
+      return superhero.id === stringId;
+    });
+  }
+
+  try {
+    return superHeroObj.name;
+  } catch (err) {
+    console.error(
+      "Please enter a number between 1 and " + superHeroNamesList.length
+    );
+  }
+};
 
 module.exports = {
-  all:superHeroNames,
-  random: uniqueRandomArray(superHeroNames)
+  rawList: superHeroNamesList,
+  allNames: allSuperHeroNames,
+  random: uniqueRandomArray(allSuperHeroNames),
+  getSuperHeroById: superHeroById
 };
